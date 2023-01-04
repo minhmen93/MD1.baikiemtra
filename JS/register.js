87b9c7242c4fbeacbd34fcbab09fe389dcab88eb
unknown <minhmen93@gmail.com>
2023-01-04 23:47:00 +0700

    hvvh

---
+++ JS/register.js
@@ -0,0 +1,10 @@
+class User{
+    username;
+    email;
+    password;
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
